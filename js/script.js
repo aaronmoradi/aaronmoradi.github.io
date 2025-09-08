2f55148d34bdeded882c6db9bbf052f50d4463ea
aaronmoradi <aaronmoradi2@gmail.com>
2025-09-08 00:37:52 -0700

    16

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 /* // tiny UX nicety: current year
 document.getElementById('year').textContent = new Date().getFullYear(); */
+if ('scrollRestoration' in history) {
+    history.scrollRestoration = 'manual';
+  }
 
 window.addEventListener('load', function() {
     window.scrollTo(1, 1);
