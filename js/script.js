08089badd318c3af6334cac4d9872da1a2808dc5
aaronmoradi <aaronmoradi2@gmail.com>
2025-09-08 00:16:16 -0700

    idk anymore

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
 /* // tiny UX nicety: current year
 document.getElementById('year').textContent = new Date().getFullYear(); */
+
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  };  
